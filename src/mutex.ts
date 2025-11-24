933f7214c50028f713f16f9131809673296fd9cd
ふぁ <yuki@yuki0311.com>
2025-11-24 22:59:40 +0000

    fix mutex

--- src/mutex.ts
+++ src/mutex.ts
@@ -2,14 +2,8 @@ import { Semaphore } from "async-mutex";
 
 export const createMutex = (value: number) => {
   const semaphore = new Semaphore(value);
-  const runExclusive = <T>(callback: () => Promise<T>): Promise<T> => {
-    return new Promise<T>((resolve) => {
-      semaphore
-        .runExclusive(async () => {
-          resolve(callback());
-        })
-        .catch((err) => resolve(err));
-    });
-  };
+	const runExclusive = <T>(callback: () => Promise<T>): Promise<T> => {
+		return semaphore.runExclusive(callback);
+	};
   return { runExclusive };
 };
